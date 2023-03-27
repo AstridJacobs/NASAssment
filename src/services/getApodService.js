b929fdb7266a135bfe41e5ee94278e5dedd64858
JacobsAstrid <128916552+JacobsAstrid@users.noreply.github.com>
2023-03-27 21:57:15 +0200

    Add home copy

--- src/services/getApodService.js
+++ src/services/getApodService.js
@@ -1,4 +1,5 @@
 const getApod = async () => {
+  //In a real application the api key should be stored in something like a key vault
   const apiKey = "VKDGLWcWUJfDwgiom0jW3S2VgrHcYMmQb1pVRoIi";
   const url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
   return fetch(url)
